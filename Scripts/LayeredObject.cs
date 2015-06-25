11104005394e4acab2752c788fbfb1a53fe510c3
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2015-06-26 01:46:35 +0900

    first

---
+++ Scripts/LayeredObject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ObjectLayer {
+	public class LayeredObject : MonoBehaviour {
+
+		public string layerName;
+
+		private ObjectLayerMaster master;
+
+		private int orgLayer;
+
+		public bool visible = true;
+
+		private int invisibleLayer;
+
+		void Awake(){
+//			orgLayer = gameObject.layer;
+			invisibleLayer = LayerMask.NameToLayer ("Invisible");
+		}
+
+		// Use this for initialization
+		void Start () {
+			master = FindObjectOfType<ObjectLayerMaster> ();
+			master.AddObject (this);
+			 
+		}
+
+		public void SetVisible(bool b){
+			visible = b;
+			if (b) {
+				gameObject.transform.SetLayer(orgLayer);
+			} else {
+				if(gameObject.layer != invisibleLayer){
+					orgLayer = gameObject.layer;
+					gameObject.transform.SetLayer(invisibleLayer);
+				}
+			}
+		}
+	}
+}
---
+++ Scripts/ObjectLayerEntry.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace ObjectLayer{
+	public class ObjectLayerEntry : MonoBehaviour {
+
+		public Text layerText;
+		public Toggle toggle;
+		
+		private string layerName;
+		private ObjectLayerMaster master;
+
+ 
+		// Use this for initialization
+		void Start () {
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public void Dispose(){
+
+		}
+
+		public void Load(){
+
+		}
+
+		public void Set(string layerName, ObjectLayerMaster master){
+			layerText.text = layerName;
+			this.layerName = layerName;
+			this.master = master;
+		}
+
+		public void SetVisible(Toggle t){
+			master.SetLayerVisible (t.isOn, layerName);
+		}
+	}
+}
